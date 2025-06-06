4088f21bb1f9e6d3f180a1db7f6bb68520a131f5
Theo Moret <theomoret.etudes@gmail.com>
2025-06-06 21:05:40 +0200

    add new_params dict to avoid inplace changes of params model(

--- optimizers/gradient_descent.py
+++ optimizers/gradient_descent.py
@@ -11,10 +11,13 @@ class GradientDescent(BaseOptimizer):
     def __init__(self, learning_rate):
         super().__init__(learning_rate)
 
-    def update(self, params, grads):
+    def step(self, params, grads):
         lr = self.lr
+        new_params = {}
         for key in params:
-            params[key] = params[key] - lr * grads[key]
+            new_params[key] = params[key] - lr * grads[key]
+        
+        return new_params
 
 
 if __name__ == "__main__":
