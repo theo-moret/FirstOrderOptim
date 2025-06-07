51fc824fd7ecac1153a93841a87cf2c17ad2e869
Theo Moret <theomoret.etudes@gmail.com>
2025-06-07 13:43:38 +0200

    create Trainer class.

--- trainer.py
+++ trainer.py
@@ -5,6 +5,78 @@ from losses.mse import MSELoss
 from models.linear_model import LinearModel
 from optimizers.gradient_descent import GradientDescent
 
+
+
+# Class
+
+class Trainer():
+
+    def __init__(self, model, loss, optimizer, n_epochs, batch_size):
+
+        self.model = model
+        self.loss = loss
+        self.optimizer = optimizer
+        self.n_epochs = n_epochs
+        self.batch_size = batch_size
+        self.loss_cache = {}
+
+
+    def train(self, X, Y, print_loss = True):
+        """
+        Set up a training loop for the model given data (X,Y). The parameter print_loss allows for loss printing at the beginning of every epoch. 
+        If the number of epoch is 1, then the losses are stocked into the cache for every batch.
+        """
+        n = len(X)
+        n_batches = n // self.batch_size
+
+
+        for epoch in range(self.n_epochs):
+            for i in range(n_batches):
+                # Fetch data
+                x = X[i * batch_size: (i+1) * batch_size]
+                y = Y[i * batch_size: (i+1) * batch_size]
+
+                # Make prediction
+                y_pred = model.forward(x)
+
+                # Loss value evaluation
+                loss_value = loss.forward(y_pred, y)
+                
+                # Print and stock loss value for evey batch if the number of epoch is 1
+                if self.n_epochs == 1:
+                    print(f"Batch {i+1} - Loss : {loss_value}")
+                    self.loss_cache[i +1] = loss_value
+
+                # Check loss at beginning of epoch and stock it in the cache
+                if i == 0 and print_loss == True:
+                    print(f"Epoch {epoch} - Loss : {loss_value}")
+                    self.loss_cache[epoch + 1] = loss_value
+
+                # Obtain gradient of the loss wrt to params
+                grads = model.backward(y, loss)
+
+                # Take gradient step
+                new_params = optimizer.step(model.params, grads)
+
+                # Update model params
+                model.update(new_params)
+
+
+        return None
+    
+
+
+    def get_losses(self):
+        """
+        Return the loss values for every epoch (or batch if the number of epoch is 1) in a numpy array.
+        """
+
+        loss_values = np.fromiter(self.loss_cache.values(), dtype=float)
+        return loss_values
+
+
+
+
 # Main
 
 if __name__ == "__main__":
@@ -27,33 +99,13 @@ if __name__ == "__main__":
     optimizer = GradientDescent(learning_rate=0.2)
 
     # Training loop
-
+    
     epochs = 200
     batch_size = 100
-    num_batches = n // batch_size
+    trainer = Trainer(model, loss, optimizer, epochs, batch_size)
 
-    for epoch in range(epochs):
-        for i in range(num_batches):
-            # Fetch data
-            x = X[i * batch_size: (i+1) * batch_size]
-            y = Y[i * batch_size: (i+1) * batch_size]
-
-            # Make prediction
-            y_pred = model.forward(x)
-
-            # Check loss at beginning of epoch
-            if i == 0:
-                print(f"Epoch {epoch} - Loss : {loss.forward(y_pred, y)}")
-
-            # Obtain gradient of the loss wrt to params
-            grads = model.backward(y, loss)
-
-            # Take gradient step
-            new_params = optimizer.step(model.params, grads)
-
-            # Update model params
-            model.update(new_params)
+    trainer.train(X,Y)
 
     # Show result 
     print(f"True params : (w,c) = ({w},{c})")
-    print(f"\n Estimated params : (w_esti, c_esti) = ({model.params['coef']},{model.params['intercept']})")
+    print(f"Estimated params : (w_esti, c_esti) = ({model.params['coef']},{model.params['intercept']})")
