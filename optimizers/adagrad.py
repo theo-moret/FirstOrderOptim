a3dd95873b12808b66086b45c28ac67bfe226370
Theo Moret <theomoret.etudes@gmail.com>
2025-06-09 21:31:14 +0200

    Implement Adagrad optimizer.

---
+++ optimizers/adagrad.py
@@ -0,0 +1,41 @@
+
+# Import 
+
+from .base import BaseOptimizer
+import numpy as np
+
+# Class
+
+class AdaGrad(BaseOptimizer):
+
+    def __init__(self, learning_rate, eps=1e-8):
+        super().__init__(learning_rate)
+        self.eps = eps
+        self.gradient_cache = {}
+
+    def step(self, params, grads):
+        """ Implement Adagrad optimizer step. """
+        
+        lr = self.lr
+        eps = self.eps
+
+
+        # Setup first value 
+        if len(self.gradient_cache) == 0:
+            for key in params:
+                self.gradient_cache[key] = grads[key]**2
+
+        # Update the gradient cache 
+        else:
+            for key in params:
+                self.gradient_cache[key] += grads[key]**2
+
+            
+        for key in params:
+            params[key] = params[key] - lr / np.sqrt(self.gradient_cache[key] + eps) * grads[key]
+        
+        return params
+
+
+if __name__ == "__main__":
+    pass
