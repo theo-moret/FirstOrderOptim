4cbac1b4de5bc5d3f1f276d55a3e72cc38246682
Theo Moret <theomoret.etudes@gmail.com>
2025-06-09 23:05:55 +0200

    Add Camel Function.

--- models/test_functions.py
+++ models/test_functions.py
@@ -51,5 +51,52 @@ class BoothFunction:
         self.params = new_params
         return None
     
+
+class ThreeHumpCamel:
+    
+    def __init__(self, x0, y0):
+
+        self.params = {
+            'x' : x0,
+            'y' : y0
+        }
+
+        return None
+
+    def forward(self):
+        """
+        Return the value of the Booth Function evaluated at (x,y)
+        """
+
+        x = self.params['x']
+        y = self.params['y']
+
+        f_value = 2*x**2 - 1.05*x**4 + x**6/6 + x*y + y**2
+
+        return f_value 
+    
+
+
+    def backward(self):
+        """
+        Return the gradient of the Booth function wrt to each parameter x and y.
+        """
+            
+        x = self.params['x']
+        y = self.params['y']
+
+        dfdx = 4*x - 4.2*x**3 + x**5 + y
+        dfdy = x + 2*y
+
+        return {'x': dfdx, 'y': dfdy}
+
+
+    def update(self, new_params):
+        """
+        Update model params to new_params.
+        """
+        self.params = new_params
+        return None 
+    
 if __name__ == "__main__":
     pass
