c8238a67f9f8ca04b08fd6a53a0bbed7d4f93bd7
Theo Moret <theomoret.etudes@gmail.com>
2025-06-09 22:31:19 +0200

    Add Booth test dunction.

---
+++ models/test_functions.py
@@ -0,0 +1,55 @@
+# Import 
+
+import numpy as np
+
+
+# Class
+
+class BoothFunction:
+    
+    def __init__(self, x0, y0):
+
+        self.params = {
+            'x' : x0,
+            'y' : y0
+        }
+
+        return None
+
+    def forward(self):
+        """
+        Return the value of the Booth Function evaluated at (x,y)
+        """
+
+        x = self.params['x']
+        y = self.params['y']
+
+        f_value = (x + 2*y - 7) ** 2 + (2*x + y - 5) ** 2
+
+        return f_value 
+    
+
+
+    def backward(self):
+        """
+        Return the gradient of the Booth function wrt to each parameter x and y.
+        """
+            
+        x = self.params['x']
+        y = self.params['y']
+
+        dfdx = 10*x + 8*y - 34
+        dfdy = 8*x + 10*y -38
+
+        return {'x': dfdx, 'y': dfdy}
+
+
+    def update(self, new_params):
+        """
+        Update model params to new_params.
+        """
+        self.params = new_params
+        return None
+    
+if __name__ == "__main__":
+    pass
